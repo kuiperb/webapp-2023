c0c06d292dbbbe5b43857265bdf55db60efd9275
kuiperb <71092147+kuiperb@users.noreply.github.com>
2023-02-21 22:44:49 +0100

    added sorting function and css

--- public/script.js
+++ public/script.js
@@ -6,13 +6,17 @@ fetch('data.json')
   .then(response => response.json())
   .then(json => {
     data = json;
-    startPage1();
+    page(0, 5);
   })
   .catch(error => console.error(error));
 
 // Generate table
 function generateTable(data, from, to) {
+
+  // Slice
   data = data.slice(from, to);
+
+  // Table
   let html = '<table><thead><tr>';
   for (let key in data[0]) {
     html += `<th id="${key}-header">${key.toUpperCase()}</th>`;
@@ -22,7 +26,7 @@ function generateTable(data, from, to) {
     html += '<tr>';
     for (let key in item) {
       if (key === 'image') {
-        html += `<td><img src="${item[key]}" alt="${item.name}" width="200" /></td>`;
+        html += `<td><img src="${item[key]}" alt="${item.name}" width="200"/></td>`;
       } else {
         html += `<td>${item[key]}</td>`;
       }
@@ -39,17 +43,56 @@ function page(from, to){
     tableContainer.innerHTML = generateTable(data, from, to);
 }
 
-// Event listeners
-var button1 = document.getElementById("page-1");
-button1.addEventListener("click", function() { page(0, 5); });
-
-var button2 = document.getElementById("page-2");
-button2.addEventListener("click", function() { page(5, 10); });
-
-var button3 = document.getElementById("page-3");
-button3.addEventListener("click", function() { page(10, 13); });
-
-// Load page at start
-function startPage1() {
+function sortByName() {
+  data.sort((a, b) => a.name - b.name);
   page(0, 5);
+}
+
+function sortByAge() {
+  data.sort((a, b) => a.age - b.age);
+  page(0, 5);
+}
+
+function sortByMood() {
+  data.sort((a, b) => a.mood.localeCompare(b.mood));
+  page(0, 5);
+}
+
+// Event listeners
+document.getElementById("page-1").addEventListener("click", () => {
+  page(0, 5);
+  document.title = "Page 1";
+});
+
+document.getElementById("page-2").addEventListener("click", () => {
+  page(5, 10);
+  document.title = "Page 2";
+});
+
+document.getElementById("page-3").addEventListener("click", () => {
+  page(10, 13);
+  document.title = "Page 3";
+});
+
+document.getElementById("name-sort").addEventListener("click", sortByName);
+document.getElementById("age-sort").addEventListener("click", sortByAge);
+document.getElementById("mood-sort").addEventListener("click", sortByMood);
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
